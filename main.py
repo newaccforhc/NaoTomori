33c77076afc684c2462314c31a867900d11cb3f7
AthK <51260976+newaccforhc@users.noreply.github.com>
2020-11-16 21:53:38 +0530

    Update prefix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from naotomori.naotomori import NaoTomori
 if __name__ == '__main__':
     if 'DISCORD_CLIENT_TOKEN' in os.environ:
         bot = NaoTomori(
-            command_prefix='!',
+            command_prefix='-:',
             description='Basic Discord bot that pings you when a new anime episode or manga chapter is released, based on your MyAnimeList account.',
             case_insensitive=True
         )
